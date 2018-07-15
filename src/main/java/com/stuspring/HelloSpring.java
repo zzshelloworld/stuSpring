f778b327f9e6078e45fc2a987bf20119d8cb6e72
zhangzhe <zhangzhe2324@gmail.com>
2018-07-16 00:45:01 +0800

    修改 log打印

--- src/main/java/com/stuspring/HelloSpring.java
+++ src/main/java/com/stuspring/HelloSpring.java
@@ -4,12 +4,25 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 /**
  * @author zhangzhe17848
  */
 @Component
 public class HelloSpring {
     private final static Logger LOGGER = LoggerFactory.getLogger(HelloSpring.class);
+
+    @PostConstruct
+    public void start() {
+        LOGGER.info("annotationBean start");
+    }
+
+    @PreDestroy
+    public void destroy() {
+        LOGGER.info("annotationBean destroy");
+    }
 
     private String name;
 
