f8784ea2035c7c45b2ede232c49ccf5691d52dca
zhangzhe17848 <87701687@qq.com>
2018-07-16 19:24:42 +0800

    学习springlifecycle

---
+++ src/main/java/com/stuspring/TestException.java
@@ -0,0 +1,14 @@
+package com.stuspring;
+
+public class TestException {
+
+    public static void main(String[] args) {
+        try {
+            System.out.print(1/0);
+            System.out.print("aaa");
+        }catch (Exception e) {
+
+        }
+
+    }
+}
---
+++ src/main/java/com/zhangzhe/springlifecycle/MyBeanPostProcessor.java
@@ -0,0 +1,26 @@
+package com.zhangzhe.springlifecycle;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+/**
+ * @author zhangzhe17848
+ */
+public class MyBeanPostProcessor implements BeanPostProcessor {
+
+    public MyBeanPostProcessor() {
+        System.out.println("【BeanPostProcessor接口】调用BeanPostProcessor的构造方法");
+    }
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("【BeanPostProcessor接口】调用postProcessBeforeInitialization方法，这里可对" + beanName + "的属性进行更改。");
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("【BeanPostProcessor接口】调用postProcessAfterInitialization方法，这里可对" + beanName + "的属性进行更改。");
+        return bean;
+    }
+}
