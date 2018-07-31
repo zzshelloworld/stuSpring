e8063c427142c5a4cd39c4a6a4e09ed5601f278c
zhangzhe <zhangzhe2324@gmail.com>
2018-08-01 01:19:54 +0800

    spring高级话题-aware-修改路径

--- src/main/java/com/zhangzhe/aware/AwareService.java
+++ src/main/java/com/zhangzhe/aware/AwareService.java
@@ -1,14 +1,15 @@
 package com.zhangzhe.aware;
 
+
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.context.ResourceLoaderAware;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+
 
 @Service
 public class AwareService implements BeanNameAware, ResourceLoaderAware {
@@ -27,16 +28,20 @@ public class AwareService implements BeanNameAware, ResourceLoaderAware {
     public void setBeanName(String name) {
         this.beanName=name;
     }
+    /**/
 
-    public void outputResult() throws IOException {
-        System.out.println("Bean的名称是:"+beanName);
-        Resource resource=loader.getResource("classpath:com/zhangzhe/aware/test.txt");
-        System.out.println("ResourceLoader加载的文件内容是:");
-        String line;
-        BufferedReader reader=new BufferedReader(new InputStreamReader(resource.getInputStream()));
-        while((line=reader.readLine())!=null){
-            System.out.println(line);
+    public void outputResult(){
+        System.out.println("Bean的名称为：" + beanName);
+
+        Resource resource =
+                loader.getResource("classpath:com/zhangzhe/aware/test.txt");
+        try{
+
+            System.out.println("ResourceLoader加载的文件内容为: " + IOUtils.toString(resource.getInputStream()));
+
+        }catch(IOException e){
+            e.printStackTrace();
         }
-        reader.close();
+
     }
 }
