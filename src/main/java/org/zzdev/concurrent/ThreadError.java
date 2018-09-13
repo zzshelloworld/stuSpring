7c85a1e210657a6b5b11180bdcf08204fde438a4
zhangzhe17848 <87701687@qq.com>
2018-09-13 18:08:14 +0800

    关于java concurrent error

--- src/main/java/org/zzdev/concurrent/ThreadError.java
+++ src/main/java/org/zzdev/concurrent/ThreadError.java
@@ -1,4 +1,34 @@
 package org.zzdev.concurrent;
 
+import java.util.concurrent.TimeUnit;
+
 public class ThreadError {
+    int count = 0;
+
+    synchronized void m() {
+        System.out.println(Thread.currentThread().getName() + "thread begin");
+        while (true) {
+            count++;
+            System.out.println(Thread.currentThread().getName() + " count:" + count);
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            if (count == 5) {
+                int i = 1 / 0;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        ThreadError t = new ThreadError();
+        new Thread(t::m, "t1").start();
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        new Thread(t::m, "t2").start();
+    }
 }
