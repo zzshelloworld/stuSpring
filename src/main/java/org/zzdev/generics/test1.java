1b19ce74ff2dd4edb44a884d018dcf60ef0b37c3
zhangzhe17848 <87701687@qq.com>
2018-10-16 17:41:46 +0800

    关于java 泛型

---
+++ src/main/java/org/zzdev/generics/test1.java
@@ -0,0 +1,15 @@
+package org.zzdev.generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// https://blog.csdn.net/briblue/article/details/76736356
+public class test1 {
+    // 类型擦除？？
+    public static void main(String[] args) {
+        List<String> l1 = new ArrayList<String>();
+        List<Integer> l2 = new ArrayList<Integer>();
+
+        System.out.println(l1.getClass() == l2.getClass());
+    }
+}
