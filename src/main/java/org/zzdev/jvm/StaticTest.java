65598a34f37139814dd4e4949317d45a3eba7e77
zhangzhe17848 <87701687@qq.com>
2018-11-09 17:42:28 +0800

    jvm file

---
+++ src/main/java/org/zzdev/jvm/StaticTest.java
@@ -0,0 +1,33 @@
+package org.zzdev.jvm;
+
+public class StaticTest {
+    public static void main(String[] args) {
+        staticFunction();
+    }
+
+    static StaticTest st = new StaticTest();
+
+    static {
+        System.out.println("1");
+    }
+
+    {
+        System.out.println("2");
+    }
+
+    StaticTest() {
+        System.out.println("3");
+        System.out.println("a=" + a + ",b=" + b);
+    }
+
+    public static void staticFunction() {
+        System.out.println("4");
+    }
+
+    int a = 110;
+    static int b = 112;
+
+    // 对static 代码块不清楚
+    // 加载 连接 初始化
+    // 3 a=110, b=0 4 1 2
+}
