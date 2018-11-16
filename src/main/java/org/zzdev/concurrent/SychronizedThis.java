f759a46736ba758722f7aa6046bec55ac8e110c5
zhangzhe17848 <87701687@qq.com>
2018-11-16 17:41:20 +0800

    jvm file

---
+++ src/main/java/org/zzdev/concurrent/SychronizedThis.java
@@ -0,0 +1,42 @@
+package org.zzdev.concurrent;
+
+import org.junit.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class SychronizedThis {
+    private int count = 10;
+
+    private void m() {
+        System.out.println("这个是mm");
+        synchronized (this) {
+            System.out.println(Thread.currentThread().getName() + " " + count);
+//            try {
+//                TimeUnit.SECONDS.sleep(100);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+        }
+    }
+
+    private void n() {
+        System.out.println("这个是nn");
+        synchronized (this) {
+            System.out.println(Thread.currentThread().getName() + " " + count);
+//            try {
+//                TimeUnit.SECONDS.sleep(100);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+        }
+    }
+
+    @Test
+    public void mn() {
+        SychronizedThis st = new SychronizedThis();
+        new Thread(st::m, "mm").start();
+        new Thread(st::n, "nan").start();
+
+
+    }
+}
